559911af7db4840869316354784f953f9c6a4e26
Pathum Dissanayake <100502665+Pathumdissanayake@users.noreply.github.com>
2024-05-10 16:05:54 +0530

    Cross Orgin

--- backend/MicroLearn/learner-service/src/main/java/com/microlearn/AdminService/controller/LearnerController.java
+++ backend/MicroLearn/learner-service/src/main/java/com/microlearn/AdminService/controller/LearnerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/learner")
+@CrossOrigin(origins = "http://localhost:3000")
 public class LearnerController {
 
     private final LearnerService learnerService;
--- frontend/src/components/learner/MyCourses.js
+++ frontend/src/components/learner/MyCourses.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function MyCourses() {
+
+    const [learners, setLearners] = useState([]); // State to store learners
+    const [error, setError] = useState(null); // State to store any errors
+
+    useEffect(() => {
+        const fetchLearners = async () => {
+        try {
+            const response = await axios.get('http://localhost:9094/learner/get-all');
+            setLearners(response.data);
+        } catch (err) {
+            setError(err.message);
+        }
+        };
+
+        fetchLearners();
+    }, []);
+
   return (
-    <div>MyCourses</div>
+    <div>
+        <div>
+            <h2>My Courses</h2>
+        </div>
+        <div>
+        <table>
+                <thead>
+                    <tr>
+                        <th>Enrollment ID</th>
+                        <th>Learner ID</th>
+                        <th>Course ID</th>
+                        <th>Payment ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {learners.map((learner) => (
+                        <tr key={learner.enrollmentId}>
+                            <td>{learner.enrollmentId}</td>
+                            <td>{learner.learnerId}</td>
+                            <td>{learner.courseId}</td>
+                            <td>{learner.paymentId}</td>
+                            <button>View Page</button>
+                            <button>Update</button>
+                            <button>Delete</button>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
+    </div>
   )
 }
 
