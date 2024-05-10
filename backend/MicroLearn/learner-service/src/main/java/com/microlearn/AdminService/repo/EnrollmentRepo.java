3a35407ae1b0664600082530698cb444f88e1409
Pathum Dissanayake <100502665+Pathumdissanayake@users.noreply.github.com>
2024-05-10 12:50:08 +0530

    Removed Enrollment CRUD

--- backend/MicroLearn/learner-service/src/main/java/com/microlearn/AdminService/repo/EnrollmentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.microlearn.AdminService.repo;
-
-import com.microlearn.AdminService.entity.Enrollment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EnrollmentRepo extends MongoRepository<Enrollment, String> {
-}
